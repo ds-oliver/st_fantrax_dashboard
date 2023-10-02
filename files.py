892b3ff3907b2e49e541a06f30ebe1b7705df524
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-10-01 19:19:55 -0700

    Updates

--- files.py
+++ files.py
@@ -5,8 +5,8 @@ import os
 # match reports 
 # data/data_out/scraped_big5_data/pl_data/all_shots_all_20230820.csv data/data_out/scraped_big5_data/pl_data/full_season_matchreports_20230820.csv
 
-shots_data = 'data/data_out/scraped_big5_data/pl_data/all_shots_all_20230924.csv'
-matches_data = 'data/data_out/scraped_big5_data/pl_data/full_season_matchreports_20230924.csv'
+shots_data = 'data/fbref-data/all_shots_all_20230924.csv'
+matches_data = 'data/fbref-data/full_season_matchreports_20230924.csv'
 
 # files paths
 data_out = 'data/data_out'
