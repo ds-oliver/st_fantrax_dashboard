fafac0dc437f5c5aefc82af585a9d5e41087086f
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-10-10 18:03:44 -0700

    Updates

--- scripts/files.py
+++ scripts/files.py
@@ -5,7 +5,7 @@ import os
 # match reports 
 # data/data_out/scraped_big5_data/pl_data/all_shots_all_20230820.csv data/data_out/scraped_big5_data/pl_data/full_season_matchreports_20230820.csv
 
-projections = 'data/projections/GW8 Projections.csv'
+projections = 'data/projections/GW9 Projections.csv'
 
 shots_data = 'data/fbref-data/all_shots_all_20231005.csv'
 new_matches_data = 'data/fbref-data/full_season_matchreports_20231005.csv'
