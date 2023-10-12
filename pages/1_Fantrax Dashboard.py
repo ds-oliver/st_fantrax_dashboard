413e780799300a0b9dfbb189407a46de5ba80a92
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-10-12 13:20:33 -0700

    Updates

--- pages/1_Fantrax Dashboard.py
+++ pages/1_Fantrax Dashboard.py
@@ -120,7 +120,7 @@ def main():
     last_gw = lastgw_df['GW'].max()
 
     # Use the cached function to display DataFrames
-    display_dataframe(lastgw_df, f"GW **:orange[{last_gw}]** Player Data", colors, divergent_colors)
+    display_dataframe(lastgw_df, f"Player Data **(:orange[GW {last_gw}])**", colors, divergent_colors)
     st.info("Note: The above table is a subset of the full player data, filtered to show only players who have played in the most recent gameweek. The overperformance metric is a simple difference of LiveRkOv (rank by Total FPts) less Ros Rank. A higher value will tell you the player is currently overperforming. HeatStreak is a 3 GW total. If HeatStreak values are missing or null, it means there was insufficient data over the last 3 gameweeks to calculate a value.")
     display_dataframe(grouped_players_df, "Player Data (All Gameweeks)", colors, divergent_colors, info_text=f"Note: This table will show the statistics earned by each respective player, across all gameweeks. At this time we are looking at **:orange[{max(lastgw_df['GW'])}]** gameweeks of data.")
     display_dataframe(team_df, "Team Data", colors, divergent_colors, info_text="Note: This table will show the statistics earned by each respective team, per game.")
