2628c0523274de166a63a03e354dcbd3fb7b7550
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-10-29 18:55:46 -0700

    e

--- pages/1_Data Dashboard.py
+++ pages/1_Data Dashboard.py
@@ -172,7 +172,7 @@ def main():
     logging.info("Starting main function")
 
     # Load the custom CSS file
-    load_cached_css(file_name="style.css")
+    load_cached_css()
 
     # Add a button to the sidebar to clear the cache
     clear_cache_button()
