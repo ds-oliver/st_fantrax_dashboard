fcf0f5d19dd952e111e37766bc7e0e8e50b8c2cd
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-11-21 22:31:18 -0800

    Update MIN to 90s in data dashboard

--- pages/1_Data Dashboard.py
+++ pages/1_Data Dashboard.py
@@ -1345,7 +1345,7 @@ def main():
                     "Attacking Stats per 90",
                     "Defensive Stats per 90",
                     "Duel Stats per 90",
-                    "MIN",
+                    "90s",
                 ]
 
                 # Radar chart specific logic
